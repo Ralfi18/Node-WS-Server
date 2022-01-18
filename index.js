2811179386be217a07e90efdb138af4182d93895
Rali Dimitrov <r.dimitrov.work@gmail.com>
2022-01-18 17:31:51 +0200

    fixes

--- index.js
+++ index.js
@@ -10,8 +10,14 @@ const mysql = require("mysql");
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 const jwt = require('jsonwebtoken');
-const secret = "someHash";
+const dotenv = require('dotenv');
+// const secret = "efd76f6cdcc8944645c073949cf34181623e64cf32b24d3933fef9029813d075183008aa3f543e471897c76978b757e947c667ad8e663753b91c5fae6ba2434b";
 // const yourPassword = "someRandomPasswordHere";
+// get config vars
+dotenv.config();
+// access config var
+const secret = process.env.TOKEN_SECRET;
+
 
 app.use(cors({
 	origin: [
@@ -21,7 +27,7 @@ app.use(cors({
 }));
 
 app.get("/", (req, res) => {
-	res.send("<h1>TEST SERVER</h1>");
+	res.send("<h1>TEST SERVER</h1>"+  require('crypto').randomBytes(64).toString('hex') );
 });
 
 app.get("/create-pass", (req, res) => {
@@ -40,6 +46,7 @@ app.get("/create-pass", (req, res) => {
 
 
 app.get("/get-user", (req, res) => {
+	// console.log( secret )
 	let user = null;
 	if(req.query.username && req.query.password) {
 		const con = mysql.createConnection({
@@ -65,10 +72,14 @@ app.get("/get-user", (req, res) => {
 			  			if(response == true) {
 			  				const token = jwt.sign({
 							  exp: Math.floor(Date.now() / 1000) + (60 * 60),
-							  data: 'foobar'
+							  data: { 
+							  	userId: result[0].id, 
+							  	userEmail: result[0].email,
+							  	name: result[0].first_name + " " + result[0].last_name
+							  }
 							}, secret);
 							con.query("UPDATE users SET token = ? WHERE id= ? LIMIT 1", [token, result[0].id], function (err, result, fields) {
-								console.log(err, result)
+								// console.log(err, result)
 							})	
 			  				user = {
 								loggedIn: true,
@@ -79,7 +90,7 @@ app.get("/get-user", (req, res) => {
 									token
 								}
 							}
-							console.log( user )
+							// console.log( user )
 							res.json(user);
 			  			} else {
 			  				res.json(user);
@@ -95,22 +106,56 @@ app.get("/get-user", (req, res) => {
 });
 
 io.on("connection", (socket) => {
+
+
 	console.log("new connection");
   	socket.on("disconnect", (reason) => {
   		console.log(reason)
   	});	
 
-  	socket.on("message", function(data) {
-  		console.log("data: ", data)
+  	socket.on("appInit", function(data){
   		// verify a token symmetric - synchronous
-		var decoded = jwt.verify(data.token, secret);
+  		// console.log( "appInit" , data)
+  		try {
+		  	const decoded = jwt.verify(data.token, secret);
+		  	console.log( decoded )
+		    //io.emit("message", msg); // Send message to sender
+	    	socket.broadcast.emit("login", "User " + decoded.data.name + " loged in!"); // Send message to everyone BUT sender
+	    	io.to(socket.id).emit('hey', 'hey')
+		} catch(err) {
+			// err
+			// console.log( err )
+			io.in(socket.id).disconnectSockets();		
+		}
+  	})
 
-		console.log(decoded) // bar
+  	socket.on("logout", function(socketID) {
+  		console.log( "Logout: ", Object.keys(io.sockets._ids));
+  // 		Object.values(io.of("/").connected).forEach(function(s) {
+  // 			console.log(s.id)
+		//     // s.disconnect(true);
+		// });
+  		if(socketID) {
 
-  		console.log(socket.id + ": " + data.msg);
+			io.in(socketID).disconnectSockets();	
+  		}
+		
+  	})
 
-	    //io.emit("message", msg); // Send message to sender
-	    socket.broadcast.emit("message", data.msg); // Send message to everyone BUT sender
+  	socket.on("message", function(data) {
+  		// verify a token symmetric - synchronous
+  		console.log( "TEST message" )
+  		try {
+		  	const decoded = jwt.verify(data.token, secret);
+  			// console.log(decoded) 
+	  		// console.log(socket.id + ": " + data.msg);
+		    //io.emit("message", msg); // Send message to sender
+	    	socket.broadcast.emit("message", data.msg); // Send message to everyone BUT sender
+		} catch(err) {
+			// err
+			// console.log( err )
+			io.in(socket.id).disconnectSockets();		
+		}
   	});
 });
 
